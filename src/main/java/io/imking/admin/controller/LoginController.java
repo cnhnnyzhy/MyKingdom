348111e3195b973e81a10412769fe3f33b07016c
cnhnnyzhy <cnhnnyzhy@163.com>
2017-12-06 21:21:51 +0800

    同步

--- src/main/java/io/imking/admin/controller/LoginController.java
+++ src/main/java/io/imking/admin/controller/LoginController.java
@@ -1,14 +1,11 @@
 package io.imking.admin.controller;
 
-<<<<<<< HEAD
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-=======
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
->>>>>>> 2bc437572f115ada5298529313654f94f24d35f3
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.imking.core.domain.Result;
